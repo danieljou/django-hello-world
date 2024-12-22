e978079444de804613f434628f43e78b486a2f25
danieljou <danielsitepro@gmail.com>
2024-12-22 21:58:53 +0100

    ASGI SERVER JWTMIDDELWARE

--- api/asgi.py
+++ api/asgi.py
@@ -3,11 +3,9 @@ from django.core.asgi import get_asgi_application
 from channels.routing  import ProtocolTypeRouter , URLRouter
 from channels.auth import AuthMiddlewareStack
 import chat.routing  
+from chat.jwt_middleware  import JWTAuthMiddleware 
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'api.settings')
-
-
-
 
 application = ProtocolTypeRouter(
     {

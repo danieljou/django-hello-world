674d488456124f5e251a8e8dd408c21f15eb8ab5
danieljou <danielsitepro@gmail.com>
2024-12-22 20:55:34 +0100

    psycopg2-binary 2

--- example/urls.py
+++ example/urls.py
@@ -1,9 +1,15 @@
-# example/urls.py
-from django.urls import path
-
-from example.views import index
+from django.conf import settings
+from django.conf.urls.static import static
+from django.contrib import admin
+from django.urls import path, include
 
 
 urlpatterns = [
-    path('', index),
+    path('admin/', admin.site.urls),
+    path('', include('chat.urls'))
 ]
+
+if settings.DEBUG:
+	urlpatterns += static(
+		settings.MEDIA_URL, document_root=settings.MEDIA_ROOT
+	)

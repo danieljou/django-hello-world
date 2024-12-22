4fc5c310a48d4d6cf95a3618c0ffcd40b951c0b7
danieljou <danielsitepro@gmail.com>
2024-12-22 21:11:38 +0100

    deleting bad folder

--- api/settings.py
+++ api/settings.py
@@ -47,7 +47,7 @@ INSTALLED_APPS = [
     'django.contrib.messages', 
     'django.contrib.staticfiles',
     'chat',
-    "exemple"
+    "example"
 ]
 
 ASGI_APPLICATION = 'chatbackend.asgi.application'

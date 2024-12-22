6683f0832f811ee75746edf3b7b01a069e409bf7
danieljou <danielsitepro@gmail.com>
2024-12-22 20:58:24 +0100

    timedelta

--- example/urls.py
+++ example/urls.py
@@ -1,15 +1,9 @@
-from django.conf import settings
-from django.conf.urls.static import static
-from django.contrib import admin
-from django.urls import path, include
+# example/urls.py
+from django.urls import path
+
+from example.views import index
 
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('chat.urls'))
+    path('', index),
 ]
-
-if settings.DEBUG:
-	urlpatterns += static(
-		settings.MEDIA_URL, document_root=settings.MEDIA_ROOT
-	)
